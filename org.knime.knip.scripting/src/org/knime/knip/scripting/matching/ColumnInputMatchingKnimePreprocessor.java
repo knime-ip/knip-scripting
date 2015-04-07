5cac81e952412c15459c123c76c16298064028ac
Squareys <Squareys@googlemail.com>
2015-04-07 13:55:26 +0200

    Use the new column to module item mapping in the preprocessor

--- org.knime.knip.scripting/src/org/knime/knip/scripting/matching/ColumnInputMatchingKnimePreprocessor.java
+++ org.knime.knip.scripting/src/org/knime/knip/scripting/matching/ColumnInputMatchingKnimePreprocessor.java
@@ -7,11 +7,12 @@ import org.knime.knip.scijava.commands.KnimePreprocessor;
 import org.knime.knip.scijava.commands.adapter.InputAdapter;
 import org.knime.knip.scijava.commands.adapter.InputAdapterService;
 import org.knime.knip.scijava.commands.impl.KnimeInputDataTableService;
+import org.knime.knip.scripting.matching.ColumnToModuleItemMappingService.ColumnToModuleItemMapping;
 import org.scijava.Priority;
+import org.scijava.log.LogService;
 import org.scijava.module.Module;
 import org.scijava.module.ModuleItem;
 import org.scijava.module.process.AbstractPreprocessorPlugin;
-import org.scijava.module.process.InitPreprocessor;
 import org.scijava.module.process.PreprocessorPlugin;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
@@ -22,54 +23,91 @@ import org.scijava.plugin.Plugin;
  *
  */
 @Plugin(type = PreprocessorPlugin.class, priority = Priority.NORMAL_PRIORITY)
-public class ColumnInputMatchingKnimePreprocessor extends AbstractPreprocessorPlugin implements KnimePreprocessor {
+public class ColumnInputMatchingKnimePreprocessor extends
+		AbstractPreprocessorPlugin implements KnimePreprocessor {
 
 	@Parameter
-	ColumnInputMatchingsService m_cimService;
-	
+	ColumnToModuleItemMappingService m_cimService;
+
 	@Parameter
 	KnimeInputDataTableService m_inputTable;
-	
+
 	@Parameter
 	InputAdapterService m_inputAdapters;
-	
+
+	@Parameter
+	LogService m_log;
+
 	@Override
 	public void process(Module module) {
-		DataTableSpec spec = m_inputTable.getInputDataTableSpec();
-		for (ModuleItem<?> i : module.getInfo().inputs()) {
-			if (!module.isResolved(i.getName())) {
-				DataRow r = m_inputTable.getInputDataRow();
 
-				String name = m_cimService.getColumnNameFor(i.getName());
-				
-				if (name == null) {
-					System.out.println("Warning: Couldn't find column input matching.");
+		// get the DataTableSpec to later find column indices
+		DataTableSpec spec = m_inputTable.getInputDataTableSpec();
+
+		// some local variables set and used in the following loop
+		ColumnToModuleItemMapping mapping = null;
+		String inputName = "";
+
+		// DataRow will remain the same while processing, this is a shortcut to
+		// it.
+		DataRow row = m_inputTable.getInputDataRow();
+
+		// try to set module input values from the current DataRow
+		for (ModuleItem<?> i : module.getInfo().inputs()) {
+			inputName = i.getName();
+
+			// the input may have already been filled by a previous
+			// preprocessor.
+			if (!module.isResolved(inputName)) {
+				// get a column to input mapping
+				mapping = m_cimService.getMappingForModuleItem(i);
+
+				// there might be no mapping for this input
+				if (mapping == null) {
+					// skip this one, it wont be resolved by this Preprocessor
+					m_log.warn("Couldn't find column input mapping for input \""
+							+ inputName + "\".");
 					continue;
 				}
-//				if (!m.getActive()) {
-//					// skip this one,  it wont be resolved by this Preprocessor
-//					System.out.println("Warning: cim is not active.");
-//					continue;
-//				}
 				
-				int index = spec.findColumnIndex(name);
-				DataCell c = r.getCell(index);
-				
-				if (c == null) {
-					cancel("Column Input Matching referred to non-existent cell.");
+				// the mapping may be inactive in which case we wont use it
+				if (!mapping.isActive()) {
+					// skip this one, it wont be resolved by this Preprocessor
+					m_log.warn("Mapping for input \"" + inputName
+							+ "\" was found, but is not active.");
+					continue;
 				}
-				
-				InputAdapter ia = m_inputAdapters.getMatchingInputAdapter(c.getClass(), i.getType());
-				
+
+				// try to get the data cell matching the mapped column
+				DataCell cell = null;
+				try {
+					cell = row.getCell(mapping.getColumnIndex(spec));
+				} catch (IndexOutOfBoundsException e) {
+					// getColumnIndex() might return -1 or a index greater the
+					// column count
+					m_log.warn("Couldn't find column \""
+							+ mapping.getColumnName()
+							+ "\" which is mapped to input " + inputName + ".");
+					continue;
+				}
+
+				// find a input adapter which can convert the cells value to the
+				// a type required by the input
+				InputAdapter ia = m_inputAdapters.getMatchingInputAdapter(
+						cell.getClass(), i.getType());
+
 				if (ia == null) {
-					cancel("No InputAdapter for: " + c.getClass() + " > " + i.getType().getCanonicalName());
+					cancel("No InputAdapter for: " + cell.getClass() + " > "
+							+ i.getType().getCanonicalName());
 					return;
 				}
-				
-				module.setInput(i.getName(), ia.getValue(c));
-				module.setResolved(i.getName(), true);
+
+				// set the input and mark resolved
+				module.setInput(inputName, ia.getValue(cell));
+				module.setResolved(inputName, true);
 			} else {
-				System.out.println("Warning: input already resolved!");
+				// Often this is not intended, so we should inform about this problem
+				m_log.warn("Input \"" + inputName + "\" was already resolved!");
 			}
 		}
 	}

52b0493004d901a1b2de4ec439bc845547f7ee60
Squareys <Squareys@googlemail.com>
2015-04-07 16:25:08 +0200

    Add javadoc

--- org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
+++ org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
@@ -7,6 +7,13 @@ import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
 import org.knime.knip.scripting.matching.ColumnToModuleItemMappingService.ColumnToModuleItemMapping;
 
 public class Util {
+	
+	/**
+	 * Fill a SettingsModelStringArray with the contents of a ColumnToModuleItemMappingService.
+	 * @param service service to fill the model with
+	 * @param model model to fill
+	 * @return model
+	 */
 	public static SettingsModelStringArray fillStringArraySettingsModel(ColumnToModuleItemMappingService service, SettingsModelStringArray model) {
 		List<ColumnToModuleItemMapping> mappings = service.getMappingsList();
 		ArrayList<String> out = new ArrayList<String>(mappings.size());

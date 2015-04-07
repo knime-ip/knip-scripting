f741c0091d3ef1309aa2d7a54e9339b539465ce0
Squareys <Squareys@googlemail.com>
2015-04-07 16:31:23 +0200

    Add method to fill a mappings service from SettingsModelStringArray
    Signed-off-by: Squareys <Squareys@googlemail.com>

--- org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
+++ org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
@@ -10,6 +10,8 @@ public class Util {
 	
 	/**
 	 * Fill a SettingsModelStringArray with the contents of a ColumnToModuleItemMappingService.
+	 * The mappings are stored as Strings in the format "columnName\ninputName".
+	 * 
 	 * @param service service to fill the model with
 	 * @param model model to fill
 	 * @return model
@@ -24,6 +26,26 @@ public class Util {
 		
 		model.setStringArrayValue(out.toArray(new String[]{}));
 		return model;
+	}
+	
+	/**
+	 * Add contents of a {@link SettingsModelStringArray} to a {@link ColumnToModuleItemMappingService}.
+	 * @param model model to get the contents from
+	 * @param service service to add the contents of model to
+	 * @return true on success, false if model contained a String not in the format "columnName\ninputName".
+	 */
+	public static boolean fillColumnToModuleItemMappingService(SettingsModelStringArray model, ColumnToModuleItemMappingService service) {
+		for (String s : model.getStringArrayValue()) {
+			String[] names = s.split("\n");
+			
+			if (names.length != 2) {
+				 // Invalid format!
+				return false;
+			} else {
+				service.addMapping(names[0], names[1]);
+			}
+		}
+		return true;
 	}
 	
 }

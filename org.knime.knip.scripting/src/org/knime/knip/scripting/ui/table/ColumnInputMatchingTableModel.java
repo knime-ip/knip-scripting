4012170385059c3ce669adc6885bc6c5703319a0
Squareys <Squareys@googlemail.com>
2015-04-07 15:43:43 +0200

    Reflect changes in colum to module item mapping system

--- org.knime.knip.scripting/src/org/knime/knip/scripting/ui/table/ColumnInputMatchingTableModel.java
+++ org.knime.knip.scripting/src/org/knime/knip/scripting/ui/table/ColumnInputMatchingTableModel.java
@@ -1,11 +1,14 @@
 package org.knime.knip.scripting.ui.table;
 
+import java.util.List;
+
 import javax.swing.table.AbstractTableModel;
 
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.config.Config;
-import org.knime.knip.scripting.matching.ColumnInputMatchingsService;
+import org.knime.knip.scripting.matching.ColumnToModuleItemMappingService;
+import org.knime.knip.scripting.matching.ColumnToModuleItemMappingService.ColumnToModuleItemMapping;
 import org.scijava.Context;
 import org.scijava.module.ModuleInfo;
 import org.scijava.plugin.Parameter;
@@ -17,27 +20,39 @@ import org.scijava.plugin.Parameter;
  */
 public class ColumnInputMatchingTableModel extends AbstractTableModel {
 
+	public static final int COLUMN = 0;
+	public static final int ACTIVE = 1;
+	public static final int INPUT = 2;
+	
 	/**
 	 * Generated serialVersionUID
 	 */
 	private static final long serialVersionUID = 6633031650341577891L;
 
 	@Parameter
-	private ColumnInputMatchingsService m_cimService;
+	private ColumnToModuleItemMappingService m_cimService;
 
 	private final DataTableSpec m_tableSpec;
 	private final ModuleInfo m_moduleInfo;
-	
-	public ColumnInputMatchingTableModel(DataTableSpec spec, ModuleInfo info, Context context) {
+
+	// reference to the m_cimServices mappings list. Should only be used for
+	// read.
+	private List<ColumnToModuleItemMapping> m_mappingsList;
+
+	public ColumnInputMatchingTableModel(DataTableSpec spec, ModuleInfo info,
+			Context context) {
 		m_tableSpec = spec;
 		m_moduleInfo = info;
-		
+
 		context.inject(this);
+
+		// reference should stay valid as long as m_cimService exists
+		m_mappingsList = m_cimService.getMappingsList();
 	}
 
 	@Override
 	public int getRowCount() {
-		return m_cimService.getMatchingsCount();
+		return m_mappingsList.size();
 	}
 
 	@Override
@@ -47,64 +62,84 @@ public class ColumnInputMatchingTableModel extends AbstractTableModel {
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		switch(columnIndex) {
-			case 0: return m_cimService.getColumnInputMatchings().get(rowIndex).getFirst();
-			case 1: return new Boolean(true);
-			case 2: return m_cimService.getColumnInputMatchings().get(rowIndex).getSecond();
-			default:
-				return new String("column indx oob");
+		switch (columnIndex) {
+		case COLUMN:
+			return m_mappingsList.get(rowIndex).getColumnName();
+		case ACTIVE:
+			return m_mappingsList.get(rowIndex).isActive();
+		case INPUT:
+			return m_mappingsList.get(rowIndex).getItemName();
+		default:
+			return new String("<Column Index Out Of Bounds!>");
 		}
 	}
-	
+
 	@Override
 	public String getColumnName(int column) {
-		switch(column){
-		case 0: return "Column";
-		case 1: return "Active";
-		case 2: return "Input";
+		switch (column) {
+		case COLUMN:
+			return "Column";
+		case ACTIVE:
+			return "Active";
+		case INPUT:
+			return "Input";
 		default:
-			return "column indx oob";
+			return "<Column Index Out Of Bounds!>";
 		}
 	}
-	
-	public void addItem(String i, String c) {
-		m_cimService.addColumnInputMatching(i, c);
+
+	public void addItem(String columnName, String inputName) {
+		m_cimService.addMapping(columnName, inputName);
 		this.fireTableDataChanged();
 	}
-	
+
 	public void removeItem(int row) {
-		m_cimService.removeColumnInputMatching(row);
-		
+		m_cimService.removeMapping(m_mappingsList.get(row));
 		this.fireTableDataChanged();
 	}
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		// all cells can be edited
 		return true;
 	}
-	
+
 	@Override
-	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		String input = m_cimService.getColumnInputMatchings().get(rowIndex).getFirst();
-		
-	}
-	
-	@Override
-	public Class<?> getColumnClass(int columnIndex) {
-		if (columnIndex == 1) {
-			return Boolean.class;
-		} else {
-			return String.class;
+	public void setValueAt(Object value, int rowIndex, int columnIndex) {
+		switch (columnIndex) {
+		case COLUMN:
+			m_mappingsList.get(rowIndex).setColumnName((String)value);
+			break;
+		case ACTIVE:
+			m_mappingsList.get(rowIndex).setActive((Boolean)value);
+			break;
+		case INPUT:
+			m_mappingsList.get(rowIndex).setItemName((String)value);
+			break;
+		default:
+			// Column index out of bounds
+			break;
 		}
 	}
-	
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		if (columnIndex == ACTIVE) {
+			return Boolean.class;
+		} else if (columnIndex == COLUMN || columnIndex == INPUT){
+			return String.class;
+		} else {
+			return Void.class;
+		}
+	}
+
 	public void loadSettingsForDialog(final Config config)
 			throws InvalidSettingsException {
-		m_cimService.loadSettingsFrom(config, m_tableSpec, m_moduleInfo);
+//		m_cimService.loadSettingsFrom(config, m_tableSpec, m_moduleInfo);
 	}
 
 	public void saveSettingsForDialog(final Config config) {
-		m_cimService.saveSettingsTo(config);
+//		m_cimService.saveSettingsTo(config);
 	}
-	
+
 };

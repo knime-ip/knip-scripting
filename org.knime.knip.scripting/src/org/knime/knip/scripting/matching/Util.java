6ea4fae36df28b899b954d83b6eb80d254628749
Squareys <Squareys@googlemail.com>
2015-04-13 12:10:53 +0200

    Store active flag with ColumnInputMappings

--- org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
+++ org.knime.knip.scripting/src/org/knime/knip/scripting/matching/Util.java
@@ -11,7 +11,7 @@ public class Util {
 	/**
 	 * Fill a SettingsModelStringArray with the contents of a
 	 * ColumnToModuleItemMappingService. The mappings are stored as Strings in
-	 * the format "columnName\ninputName".
+	 * the format "columnName\nactive\ninputName".
 	 * 
 	 * @param service
 	 *            service to fill the model with
@@ -26,7 +26,9 @@ public class Util {
 		ArrayList<String> out = new ArrayList<String>(mappings.size());
 
 		for (ColumnToModuleItemMapping m : mappings) {
-			out.add(m.getColumnName() + "\n" + m.getItemName());
+			out.add(m.getColumnName() + "\n"
+					+ (m.isActive() ? "true" : "false") + "\n" 
+					+ m.getItemName());
 		}
 
 		model.setStringArrayValue(out.toArray(new String[] {}));
@@ -42,7 +44,7 @@ public class Util {
 	 * @param service
 	 *            service to add the contents of model to
 	 * @return true on success, false if model contained a String not in the
-	 *         format "columnName\ninputName".
+	 *         format "columnName\nactive\ninputName".
 	 */
 	public static boolean fillColumnToModuleItemMappingService(
 			SettingsModelStringArray model,
@@ -50,11 +52,14 @@ public class Util {
 		for (String s : model.getStringArrayValue()) {
 			String[] names = s.split("\n");
 
-			if (names.length != 2) {
+			if (names.length != 3) {
 				// Invalid format!
 				return false;
 			} else {
-				service.addMapping(names[0], names[1]);
+				ColumnToModuleItemMapping mapping = service.addMapping(names[0], names[2]);
+				if (names[1].equals("false")) {
+					mapping.setActive(false);
+				} // else: keep default true
 			}
 		}
 		return true;

7e9c80f993d00c4903cbc8c767043bf36cb83a63
Squareys <Squareys@googlemail.com>
2015-10-08 15:18:22 +0200

    Add CompileHelper and CompileProductHelper
    Will very likely be renamed in the future.
    
    Signed-off-by: Squareys <Squareys@googlemail.com>

---
+++ org.knime.knip.scripting.base/src/org/knime/knip/scripting/base/CompileProductHelper.java
@@ -0,0 +1,25 @@
+package org.knime.knip.scripting.base;
+
+import org.scijava.module.Module;
+import org.scijava.module.ModuleException;
+import org.scijava.module.ModuleInfo;
+import org.scijava.module.ModuleItem;
+import org.scijava.script.ScriptLanguage;
+
+/**
+ * Interface for classes which wrap a compilation product. This may be a
+ * CommandInfo (for Java scripts) or a ScriptInfo (basically everything else.
+ * 
+ * @author Jonathan Hale
+ */
+public interface CompileProductHelper {
+
+	public Iterable<ModuleItem<?>> inputs();
+
+	public ModuleInfo getModuleInfo();
+
+	public Module createModule(ScriptLanguage language) throws ModuleException;
+	
+	public void resetModule(Module m);
+
+}
